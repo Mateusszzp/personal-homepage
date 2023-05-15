6f9a681e78cf841da33b35e8ed74f3dc3348fed9
Mateusszzp <mateusz.pasko.r@gmail.com>
2023-05-15 18:18:05 +0200

    extracting the folder Header

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import { Article, Image, Section, Title, A, Button, Span, Wrapper } from "./components/Header";
-import mateuszP from "./image/mateuszP.jpg"
-import toggleMode from "./image/ToggleMode.png"
+import Header from "./Header";
+
+
 
 
 
 function App() {
   return (
-
-    <>
-    <Section>
-        <Image src={mateuszP} alt="" />
-        <Button > DARK MODE OFF<img src={toggleMode} alt="" /></Button>
-        <Wrapper>
-        
-          <Span>This is</Span>
-          <Title>Mateusz Paśko</Title>
-          <Article>⚒⏳ Progamowanie jest dla mnie nowym doświadczeniem i przyjemność. Odnalazłem swoje miejsce w IT</Article>
-          <A href="mailto:mateusz.pasko.r@gmail.com">✉ Hire me</A>
-        </Wrapper>
-      </Section>
-      </>
-
-
-  );
-}
+    <Header />
+  )
+};
 
 export default App;
